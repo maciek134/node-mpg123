db3380150cb2494c7dc90ac359fa72c71f847458
maciek134 <maciek134@gmail.com>
2012-10-17 00:05:23 +0200

    Added d event emitter.

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ Entirely JS based mpg123 (http://www.mpg123.de/) wrapper for Node.js.
 Example Usage
 ------------
 
-````
+````javascript
 var Sound = require('mpg123');
 
 // fire and forget:
 new Sound('/path/to/the/file/filename.mp3').play();
+
+
 // with ability to pause/resume:
 var music = new Sound('/path/to/the/file/filename.mp3');
 music.play();
--- index.js
+++ index.js
@@ -8,14 +8,38 @@
  * MIT License
  */
  
+var	spawn = require('child_process').spawn,
+	events = require('events'),
+	util = require('util');
+ 
 module.exports = function Sound(filename) {
+	events.EventEmitter.call(this);
 	this.filename = filename;
 };
-module.exports.prototype = {
-	play: function () {
-		this.process = require('child_process').spawn('mpg123', [ this.filename ]);
-	},
-	stop: function () { this.process.kill('SIGTERM'); },
-	pause: function () { this.process.kill('SIGSTOP'); },
-	resume: function () { this.process.kill('SIGCONT'); }
+
+util.inherits(module.exports, events.EventEmitter);
+
+module.exports.prototype.play = function () {
+	this.stopped = false;
+	this.process = spawn('mpg123', [ this.filename ]);
+	this.process.on('exit', function (code, sig) {
+		if (code !== null && sig === null) {
+			this.emit('completed');
+		}
+	});
+};
+module.exports.prototype.stop = function () {
+	this.stopped = true;
+	this.process.kill('SIGTERM');
+	this.emit('stop');
+};
+module.exports.prototype.pause = function () {
+	if (this.stopped) return;
+	this.process.kill('SIGSTOP');
+	this.emit('pause');
+};
+module.exports.prototype.resume = function () {
+	if (this.stopped) return this.play();
+	this.process.kill('SIGCONT');
+	this.emit('resume');
 };
